ebc7faaa92ebfc52db77dfdc234ebba2ff3a94ec
Simana <poudelsimana22@gmail.com>
2025-02-10 16:42:31 +0545

    feat: github and linkend added

--- assets/js/team.js
+++ assets/js/team.js
@@ -4,24 +4,39 @@ document.addEventListener("DOMContentLoaded", function () {
   if (teamContainer && window.teamData) {
     const teamHTML = `
     <div class="team-container-wrapper">
-          <h2 class="team-heading">Meet Our Talented Team</h2>
-          <p class="team-desc">Our team is a diverse group of experts who bring a wide range of skills, from creative design to technical development, strategic planning, and problem-solving. Together, we collaborate to turn ideas into innovative solutions that drive our success.</p>
-          <div class="grid">
-              ${window.teamData
-                .map(
-                  (member) => `
-                          <div class="card">
-                              <img src="${member.image}" alt="${member.name}" class="team-img">
-                              <h3>${member.name}</h3>
-                              <p class="team-role">${member.role}</p>
-                              <p class="team-info">${member.description}</p>
-                          </div>
-                      `
-                )
-                .join("")}
-          </div>
+      <h2 class="team-heading">Meet Our Talented Team</h2>
+      <p class="team-desc">Our team is a diverse group of experts who bring a wide range of skills, from creative design to technical development, strategic planning, and problem-solving. Together, we collaborate to turn ideas into innovative solutions that drive our success.</p>
+      <div class="grid">
+        ${window.teamData
+          .map(
+            (member) => `
+              <div class="card">
+                <img src="${member.image}" alt="${
+              member.name
+            }" class="team-img">
+                <h3>${member.name}</h3>
+                <p class="team-role">${member.role}</p>
+                <p class="team-info">${member.description}</p>
+                <div class="social-links">
+  ${
+    member.github
+      ? `<a href="${member.github}" target="_blank" class="github"></a>`
+      : ""
+  }
+  ${
+    member.linkedin
+      ? `<a href="${member.linkedin}" target="_blank" class="linkedin"></a>`
+      : ""
+  }
+</div>
+
+              </div>
+            `
+          )
+          .join("")}
       </div>
-      `;
+    </div>
+    `;
     teamContainer.innerHTML = teamHTML;
   }
 });
--- assets/js/teams-data.js
+++ assets/js/teams-data.js
@@ -5,6 +5,8 @@ window.teamData = [
     description:
       "A visionary leader committed to driving business growth through innovation and strategic partnerships.",
     image: "./assets/images/teams/placeholder.webp",
+    github: "https://github.com/techgaun",
+    linkedin: "https://www.linkedin.com/in/techgaun/",
   },
   {
     name: "Brisha Pote",
@@ -12,6 +14,8 @@ window.teamData = [
     description:
       "A results-driven professional dedicated to fostering a collaborative work environment and ensuring product excellence.",
     image: "./assets/images/teams/placeholder.webp",
+    github: "https://github.com/bpote",
+    linkedin: "https://www.linkedin.com/in/brisha-pote-201bab1b3/",
   },
   {
     name: "Rajan Midun Magar",
@@ -19,6 +23,8 @@ window.teamData = [
     description:
       "Expert in Node.js, React.js, RESTful APIs, and server-side solutions for scalable applications.",
     image: "./assets/images/teams/placeholder.webp",
+    github: "https://github.com/rznmgr630",
+    linkedin: "https://www.linkedin.com/in/rajan-midun-magar-60aa3b19b/",
   },
   {
     name: "Simana Poudel",
@@ -26,6 +32,8 @@ window.teamData = [
     description:
       "Specializes in crafting responsive, scalable web applications using React and Next.js.",
     image: "./assets/images/teams/simanapp.jpg",
+    github: "https://github.com/Simana-poudel",
+    linkedin: "https://www.linkedin.com/in/simana-poudel-16b36a146/",
   },
   {
     name: "Anusha Gurung",
@@ -33,12 +41,7 @@ window.teamData = [
     description:
       "Passionate about creating user-centric designs that enhance customer experiences.",
     image: "./assets/images/teams/placeholder.webp",
-  },
-  {
-    name: "Anusha Gurung",
-    role: "UI/UX Designer",
-    description:
-      "Passionate about creating user-centric designs that enhance customer experiences.",
-    image: "./assets/images/teams/placeholder.webp",
+    github: "https://github.com/anushagurung",
+    linkedin: "https://www.linkedin.com/in/anushagurung/",
   },
 ];
