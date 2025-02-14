1fc0e177b5142deb039d5b0d94e155d1679ea068
techgaun <coolsamar207@gmail.com>
2025-02-14 01:22:33 -0600

    feat: update images

--- assets/js/teams-data.js
+++ assets/js/teams-data.js
@@ -1,10 +1,10 @@
 window.teamData = [
   {
     name: "Samar Acharya",
-    role: "CEO",
+    role: "CEO, Founder at large",
     description:
       "A visionary leader committed to driving business growth through innovation and strategic partnerships.",
-    image: "./assets/images/team/samar.webp",
+    image: "./assets/images/team/placeholder.png",
     github: "https://github.com/techgaun",
     linkedin: "https://www.linkedin.com/in/techgaun/",
   },
@@ -13,7 +13,7 @@ window.teamData = [
     role: "CPO",
     description:
       "A results-driven professional dedicated to fostering a collaborative work environment and ensuring product excellence.",
-    image: "./assets/images/team/guy.jpg",
+    image: "./assets/images/team/brisha.jpg",
     github: "https://github.com/bpote",
     linkedin: "https://www.linkedin.com/in/brisha-pote-201bab1b3/",
   },
@@ -22,7 +22,7 @@ window.teamData = [
     role: "Full-Stack Developer",
     description:
       "Expert in Node.js, React.js, RESTful APIs, and server-side solutions for scalable applications.",
-    image: "./assets/images/team/guy.jpg",
+    image: "./assets/images/team/rajan.png",
     github: "https://github.com/rznmgr630",
     linkedin: "https://www.linkedin.com/in/rajan-midun-magar-60aa3b19b/",
   },
@@ -31,7 +31,7 @@ window.teamData = [
     role: "Frontend Developer",
     description:
       "Specializes in crafting responsive, scalable web applications using React and Next.js.",
-    image: "./assets/images/team/guy.jpg",
+    image: "./assets/images/team/simana.png",
     github: "https://github.com/Simana-poudel",
     linkedin: "https://www.linkedin.com/in/simana-poudel-16b36a146/",
   },
@@ -57,7 +57,7 @@ window.teamData = [
     role: "Junior Engineer",
     description:
       "A dedicated junior engineer contributing to development projects.",
-    image: "./assets/images/team/guy.jpg",
+    image: "./assets/images/team/sagar.jpg",
     github: "",
     linkedin: "",
   },
