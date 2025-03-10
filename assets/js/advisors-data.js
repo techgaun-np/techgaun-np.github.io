7136191f158a5edc84f7725dd428ffdc5d2473d5
Simana <poudelsimana22@gmail.com>
2025-03-10 11:35:44 +0545

    feat: add advisor

---
+++ assets/js/advisors-data.js
@@ -0,0 +1,11 @@
+window.advisorsData = [
+  {
+    name: "Sindhu Jalesa Budhathoki",
+    role: "Advisor",
+    description:
+      "Constituent Assembly Member, Educator, Folk Singer, Song-Writer, and Advocate of Social Service and Hands-On Philanthropist.",
+    image: "./assets/images/team/sindhu.png",
+    github: "",
+    linkedin: "",
+  },
+];
