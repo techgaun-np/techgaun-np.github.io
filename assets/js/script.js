8a38a42d9269bb5d45c6409c774cdf2073b5e147
Simana <poudelsimana22@gmail.com>
2025-07-15 11:53:22 +0545

    feat: banner modal

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,77 +1,100 @@
 //scroll to top
 window.onscroll = function () {
-    var scrollButton = document.getElementById('scroll-top');
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-        scrollButton.style.display = 'block';
-    } else {
-        scrollButton.style.display = 'none';
-    }
+  var scrollButton = document.getElementById("scroll-top");
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    scrollButton.style.display = "block";
+  } else {
+    scrollButton.style.display = "none";
+  }
 };
 
 function scrollToTop() {
-    window.scroll({ top: 0, behavior: 'smooth' })
+  window.scroll({ top: 0, behavior: "smooth" });
 }
 
 //responsive navigation
 document.addEventListener("click", function (e) {
-    var navIcon = document.getElementById("nav-icon");
-    var mainNav = document.getElementById("main-nav");
+  var navIcon = document.getElementById("nav-icon");
+  var mainNav = document.getElementById("main-nav");
 
-    if (navIcon.contains(e.target)) {
-        toggleNav(mainNav);
-    } else {
-        closeNav(mainNav);
-    }
+  if (navIcon.contains(e.target)) {
+    toggleNav(mainNav);
+  } else {
+    closeNav(mainNav);
+  }
 });
 
 function closeNav(element) {
-    element.className = "main-nav";
+  element.className = "main-nav";
 }
 
 function toggleNav(element) {
-    if (element.className === "main-nav") {
-        element.className += " responsive";
-    } else {
-        closeNav(element);
-    }
+  if (element.className === "main-nav") {
+    element.className += " responsive";
+  } else {
+    closeNav(element);
+  }
 }
 
 // Initialize emailjs
 (function () {
-    emailjs.init({
-        publicKey: "-j9iqbK7XwOzki-g6",
-    });
+  emailjs.init({
+    publicKey: "-j9iqbK7XwOzki-g6",
+  });
 })();
 
 document.addEventListener("DOMContentLoaded", function () {
-    // Contact form submission section
-    var contactForm = document.getElementById('contact-form');
-    var successMessage = document.getElementById('success-message');
+  // Contact form submission section
+  var contactForm = document.getElementById("contact-form");
+  var successMessage = document.getElementById("success-message");
 
-    contactForm.addEventListener('submit', function (event) {
-        event.preventDefault();
+  contactForm.addEventListener("submit", function (event) {
+    event.preventDefault();
 
-        var submitButton = contactForm.querySelector('button[type="submit"]');
-        submitButton.disabled = true;
-        submitButton.textContent = 'Sending...';
+    var submitButton = contactForm.querySelector('button[type="submit"]');
+    submitButton.disabled = true;
+    submitButton.textContent = "Sending...";
 
-        emailjs.sendForm('service_008lxmj', 'template_wiuzc5o', this)
-            .then(function () {
-                contactForm.reset();
-                // contactForm.style.display = 'none';
-                successMessage.style.display = 'block';
+    emailjs.sendForm("service_008lxmj", "template_wiuzc5o", this).then(
+      function () {
+        contactForm.reset();
+        // contactForm.style.display = 'none';
+        successMessage.style.display = "block";
 
-                setTimeout(function () {
-                    successMessage.style.display = 'none';
-                    // contactForm.style.display = 'block';
-                    submitButton.disabled = false;
-                    submitButton.textContent = 'Submit';
-                }, 5000);
-            }, function (error) {
-                // console.log('FAILED...', error);
-                // alert('Failed to send the message. Please try again later.');
-                submitButton.disabled = false;
-                submitButton.textContent = 'Submit';
-            });
-    });
+        setTimeout(function () {
+          successMessage.style.display = "none";
+          // contactForm.style.display = 'block';
+          submitButton.disabled = false;
+          submitButton.textContent = "Submit";
+        }, 5000);
+      },
+      function (error) {
+        // console.log('FAILED...', error);
+        // alert('Failed to send the message. Please try again later.');
+        submitButton.disabled = false;
+        submitButton.textContent = "Submit";
+      }
+    );
+  });
 });
+
+window.addEventListener("DOMContentLoaded", () => {
+  const modal = document.getElementById("initialModal");
+  if (modal) {
+    modal.style.display = "flex";
+  }
+});
+
+function closeModal() {
+  const modal = document.getElementById("initialModal");
+  if (modal) {
+    modal.style.display = "none";
+  }
+}
+
+function closeBanner() {
+  const banner = document.getElementById("announcementBanner");
+  if (banner) {
+    banner.style.display = "none";
+  }
+}
