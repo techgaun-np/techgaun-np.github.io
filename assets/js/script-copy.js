6cd6c1c0eb54a6a8017c37e8ce957e07d93130a7
Simana <poudelsimana22@gmail.com>
2025-07-17 13:42:49 +0545

    fea: update case study with crousel

--- assets/js/script-copy.js
+++ assets/js/script-copy.js
@@ -36,69 +36,6 @@ function toggleNav(element) {
   }
 }
 
-// Initialize emailjs
-(function () {
-  emailjs.init({
-    publicKey: "-j9iqbK7XwOzki-g6",
-  });
-})();
-
-document.addEventListener("DOMContentLoaded", function () {
-  // Contact form submission section
-  var contactForm = document.getElementById("contact-form");
-  var successMessage = document.getElementById("success-message");
-
-  contactForm.addEventListener("submit", function (event) {
-    event.preventDefault();
-
-    var submitButton = contactForm.querySelector('button[type="submit"]');
-    submitButton.disabled = true;
-    submitButton.textContent = "Sending...";
-
-    emailjs.sendForm("service_008lxmj", "template_wiuzc5o", this).then(
-      function () {
-        contactForm.reset();
-        // contactForm.style.display = 'none';
-        successMessage.style.display = "block";
-
-        setTimeout(function () {
-          successMessage.style.display = "none";
-          // contactForm.style.display = 'block';
-          submitButton.disabled = false;
-          submitButton.textContent = "Submit";
-        }, 5000);
-      },
-      function (error) {
-        // console.log('FAILED...', error);
-        // alert('Failed to send the message. Please try again later.');
-        submitButton.disabled = false;
-        submitButton.textContent = "Submit";
-      }
-    );
-  });
-});
-
-window.addEventListener("DOMContentLoaded", () => {
-  const modal = document.getElementById("initialModal");
-  if (modal) {
-    modal.style.display = "flex";
-  }
-});
-
-function closeModal() {
-  const modal = document.getElementById("initialModal");
-  if (modal) {
-    modal.style.display = "none";
-  }
-}
-
-function closeBanner() {
-  const banner = document.getElementById("announcementBanner");
-  if (banner) {
-    banner.style.display = "none";
-  }
-}
-
 // Carousel functionality
 document.addEventListener("DOMContentLoaded", () => {
   const imageTrack = document.querySelector(".carousel-image-track");
